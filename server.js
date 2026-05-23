075f4793adcaeb05fdb8546ebd21420948d3485e
Maksim <maksim@example.me>
2026-05-23 17:26:02 +0200

    remade string array of objects

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+const fs = require(`fs`).promises
 const http = require("node:http")
 
 
+fs.readFile('./storage.txt', 'utf8')
+    .then((data) => {
+	console.log("[", data, "]")
+	const board = [];
+	const lines = data.split("\n");
+	for (let i = 0; i < lines.length - 1; i++) {
+	    console.log("line=", lines[i])
+	    const [name, score] = lines[i].split(":")
+	    board.push({ name: name, score: score });
+	}
+		
+	// parse data to board
+	console.log("board", board)
+	const html = renderScoreBoard(board)
+	console.log("html", html)
 
-const scoreBoard = [
-    {name: "Maksim", score: 9999},
-    {name: "Mark", score: 10},
-    {name: "Andrey", score: 666},
-]
+	let problem = "turning string to an object"
+    })
+    .catch((error) => {
+	// This single catch handles errors from BOTH readFile and writeFile
+	console.error('An error occurred during the file operations:', error.message);
+    });
 
 function renderScoreBoard(board) {
     let scoreBoardHtml = `
@@ -32,9 +49,9 @@ http
     .createServer((req, res) => {
 	console.log("we got a request", "request", req, "response", res)
 	res.writeHead(200, { "Content-Type": "text/html" })
-	const html = renderScoreBoard(scoreBoard)
+	const html = renderScoreBoard()
 	res.end(html)
     })
-    .listen(8080)
+//    .listen(8080)
 
 console.log("we are up and running")
