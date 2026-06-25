f801e0f4361aad72edaa1a5477716b1a966a7cfc
Maksim <maksim@example.me>
2026-06-25 15:37:57 +0200

    fixed few things in the rework of the server

---
+++ server.js
@@ -0,0 +1,85 @@
+//reads scoreboard...
+//writes scoreboard...
+//returns game as well...
+// Import the built-in HTTP module
+const http = require('node:http');
+const { readFile } = require('node:fs/promises');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 3000;
+
+
+
+// Persistimg layer
+function readScoreBoardFromFile() {
+    return readFile('storage.json', 'utf8')
+	.then((json) => {
+	    return JSON.parse(json)
+	})
+}
+
+function writeScoreBoardToFile(scoreBoard) {
+    
+}
+
+// Business logic
+function updateUserScoreIfNeeded(name, score) {
+    
+}
+
+// web-layer
+function handleIndex(req, res) {
+    readFile('index.html', 'utf8')
+    	.catch(err => {
+	    console.error("failed to read index.html with", err)
+	    res.writeHeader(500, { "Content-Type": "text/plain; charset=utf-8"})
+	    res.end("life sucks: " + err) 
+	})
+	.then(html => {
+	    res.writeHeader(200, { "Content-Type": "text/html; charset=utf-8"})
+	    res.end(html)
+	})
+}
+
+function handleScoreBoard(req, res) {
+    readScoreBoardFromFile()
+    	.catch((error) => {
+	    console.error("failed to read scoreboard with", error)
+	    res.writeHeader(500, {"Content-Type": "text/html; charset=utf-8"})
+	    res.end("failed to read coreBoard")
+	})
+	.then(scoreBoard => {
+	    console.log("scoreBoard", scoreBoard["maksim"])
+	    res.writeHeader(200, { "Content-Type": "text/html; charset=utf-8"})
+	    res.end("<p>not implemented yet</p>")
+	})
+}
+
+function handleRegister() {
+    
+}
+
+const handleNotFound = (req, res) => {
+    res.writeHeader(404, { "Content-Type": "text/html; charset=utf-8"})
+    res.end(`<p>Route ${req.url} not found</p>`)
+}
+
+const router = {
+    "/": handleIndex,
+    "/score-board": handleScoreBoard,
+}
+
+// run the server
+const server = http.createServer((req, res) => {
+    let handler = router[req.url]
+    if (handler == null) {
+	handler = handleNotFound
+    }
+    handler(req, res)
+    console.log("got request to url:", req.url, "response status", res.statusCode)
+});
+
+// Start the server on the specified port and hostname
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
